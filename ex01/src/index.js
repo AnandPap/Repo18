9816c48e330c7becc7a213d2109d87e9612e1bbb
Anand Pap <anandpap@live.com>
2021-10-01 00:39:55 +0200

    Generator functions

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+let myArr = [];
+// Only change code below this line
+function* multiplication(x) {
+    for (let i = 0; i < x; i++) {
+        x = x * 2;
+        myArr.push(x);
+        yield x;
+    }
+}
+// Only change code above this line
+var num = multiplication(3);
+for (let i = 0; i < 3; i++) {
+    console.log(num.next().value);
+}
+console.log(myArr);
+module.exports = multiplication;
