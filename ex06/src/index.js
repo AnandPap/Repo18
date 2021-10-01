07868ca5bbd359228824dd52ab0b41aff4e0ff24
Anand Pap <anandpap@live.com>
2021-10-01 08:42:21 +0200

    Spreading iterables

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -2,10 +2,23 @@
 let rockStar = new Map ([
     ["artist", "The Rolling Stones"],
     ["song", "Angie"],
-    ["album", "Goast Head Soup"],
+    ["album", "Goats Head Soup"],
     ["singer", "Mick Jagger"]
 ]);
 
-let newRock = 
+let array = [...rockStar.keys()];
+let iterator1 = rockStar.keys();
+let iterator2 = rockStar.values();
+
+let newRock = new Map ([]);
+for (let i = 0; i < 4; i++) {
+    if (array[i].charAt(0) > 'a') {
+        newRock.set(iterator1.next().value, iterator2.next().value);
+    } else {
+        iterator1.next().value;
+        iterator2.next().value;
+    }
+}
+console.log(newRock);
 // Only change code above this line
 module.exports = { rockStar, newRock };
