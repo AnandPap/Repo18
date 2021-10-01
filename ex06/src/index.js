7bfc0ab76ea6dc56fa0699ff1877712a67dad8b7
Anand Pap <anandpap@live.com>
2021-10-01 02:32:10 +0200

    Spreading iterables

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+let rockStar = new Map ([
+    ["artist", "The Rolling Stones"],
+    ["song", "Angie"],
+    ["album", "Goast Head Soup"],
+    ["singer", "Mick Jagger"]
+]);
+
+let newRock = 
+// Only change code above this line
+module.exports = { rockStar, newRock };
